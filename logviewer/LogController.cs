ee98b42574ed021f3190b3961506ea3567212a46
egoroff <egoroff@gmail.com>
2012-09-16 13:46:42 +0400

    Async file reading

--- logviewer/LogController.cs
+++ logviewer/LogController.cs
@@ -4,16 +4,19 @@ using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace logviewer
 {
     public class LogController
     {
-        private const int MeanLogStringLength = 70;
-        private const int BomLength = 3; // BOM (Byte Order Mark)
-        private const string StartMessagePattern = @"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}.*";
+        #region Constants and Fields
+
         private const string BigFileFormat = "{0:F2} {1} ({2} {3})";
+        private const int BomLength = 3; // BOM (Byte Order Mark)
+        private const int MeanLogStringLength = 70;
         private const string SmallFileFormat = "{0} {1}";
+        private const string StartMessagePattern = @"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}.*";
         private static readonly Regex regex = new Regex(StartMessagePattern, RegexOptions.Compiled);
 
         private static readonly string[] sizes = new[]
@@ -29,15 +32,56 @@ namespace logviewer
 
         private readonly ILogView view;
 
+        #endregion
+
+        #region Constructors and Destructors
+
         public LogController(ILogView view)
         {
             this.view = view;
-            Executive.SafeRun(this.Convert);
+            Executive.SafeRun(Convert);
         }
 
-        public long LogSize { get; private set; }
+        #endregion
+
+        #region Public Properties
 
         public string HumanReadableLogSize { get; private set; }
+        public long LogSize { get; private set; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public static async Task<string> ReadLog(Stream stream)
+        {
+            var messages = new List<LogMessage>((int) stream.Length/MeanLogStringLength);
+            var message = new LogMessage {Strings = new List<string>()};
+            long length;
+            using (var sr = new StreamReader(stream, Encoding.UTF8, true))
+            {
+                length = stream.Length;
+                while (!sr.EndOfStream)
+                {
+                    var line = await sr.ReadLineAsync();
+
+                    if (regex.IsMatch(line) && message.Strings.Count > 0)
+                    {
+                        messages.Add(message);
+                        message.Strings = new List<string>();
+                    }
+
+                    message.Strings.Add(line);
+                }
+            }
+            messages.Add(message);
+            var sb = new StringBuilder((int)length + messages.Count * Environment.NewLine.Length);
+            for (int i = messages.Count - 1; i >= 0; --i)
+            {
+                sb.Append(messages[i]);
+            }
+            return sb.ToString();
+        }
 
         public string ReadLog(string path)
         {
@@ -50,10 +94,10 @@ namespace logviewer
                 var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
                 using (stream)
                 {
-                    this.LogSize = stream.Length;
-                    this.CreateHumanReadableSize();
+                    LogSize = stream.Length;
+                    CreateHumanReadableSize();
 
-                    return ReadLog(stream);
+                    return ReadLog(stream).Result;
                 }
             }
             catch (Exception e)
@@ -62,57 +106,17 @@ namespace logviewer
             }
         }
 
-        private void CreateHumanReadableSize()
-        {
-            var normalized = new FileSize((ulong) this.LogSize);
-            if (normalized.Unit == SizeUnit.Bytes)
-            {
-                this.HumanReadableLogSize = string.Format(CultureInfo.CurrentCulture, SmallFileFormat, normalized.Bytes,
-                                                          sizes[(int) normalized.Unit]);
-            }
-            else
-            {
-                this.HumanReadableLogSize = string.Format(CultureInfo.CurrentCulture, BigFileFormat, normalized.Value,
-                                                          sizes[(int) normalized.Unit], normalized.Bytes,
-                                                          sizes[(int) SizeUnit.Bytes]);
-            }
-        }
+        #endregion
 
-        public static string ReadLog(Stream stream)
-        {
-            using (var sr = new StreamReader(stream, Encoding.UTF8, true))
-            {
-                var messages = new List<LogMessage>((int) stream.Length / MeanLogStringLength);
-                var message = new LogMessage { Strings = new List<string>() };
-                while (!sr.EndOfStream)
-                {
-                    string line = sr.ReadLine();
-
-                    if (regex.IsMatch(line) && message.Strings.Count > 0)
-                    {
-                        messages.Add(message);
-                        message.Strings = new List<string>();
-                    }
-
-                    message.Strings.Add(line);
-                }
-                messages.Add(message);
-                var sb = new StringBuilder((int) stream.Length + messages.Count * Environment.NewLine.Length);
-                for (int i = messages.Count - 1; i >= 0; --i)
-                {
-                    sb.Append(messages[i]);
-                }
-                return sb.ToString();
-            }
-        }
+        #region Methods
 
         private void Convert()
         {
-            if (!File.Exists(this.view.LogPath))
+            if (!File.Exists(view.LogPath))
             {
                 return;
             }
-            FileStream stream = File.Open(this.view.LogPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            FileStream stream = File.Open(view.LogPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
             byte[] b;
             using (stream)
             {
@@ -124,26 +128,52 @@ namespace logviewer
                 return;
             }
             Encoding srcEncoding = Encoding.GetEncoding("windows-1251");
-            string log = File.ReadAllText(this.view.LogPath, srcEncoding);
+            string log = File.ReadAllText(view.LogPath, srcEncoding);
             byte[] asciiBytes = srcEncoding.GetBytes(log);
             byte[] utf8Bytes = Encoding.Convert(srcEncoding, Encoding.UTF8, asciiBytes);
             string utf8 = Encoding.UTF8.GetString(utf8Bytes);
-            File.WriteAllText(this.view.LogPath, utf8, Encoding.UTF8);
+            File.WriteAllText(view.LogPath, utf8, Encoding.UTF8);
         }
+
+        private void CreateHumanReadableSize()
+        {
+            var normalized = new FileSize((ulong) LogSize);
+            if (normalized.Unit == SizeUnit.Bytes)
+            {
+                HumanReadableLogSize = string.Format(CultureInfo.CurrentCulture, SmallFileFormat, normalized.Bytes,
+                                                     sizes[(int) normalized.Unit]);
+            }
+            else
+            {
+                HumanReadableLogSize = string.Format(CultureInfo.CurrentCulture, BigFileFormat, normalized.Value,
+                                                     sizes[(int) normalized.Unit], normalized.Bytes,
+                                                     sizes[(int) SizeUnit.Bytes]);
+            }
+        }
+
+        #endregion
     }
 
     internal struct LogMessage
     {
+        #region Constants and Fields
+
         internal IList<string> Strings;
+
+        #endregion
+
+        #region Public Methods and Operators
 
         public override string ToString()
         {
-            StringBuilder sb = new StringBuilder();
-            foreach (string s in this.Strings)
+            var sb = new StringBuilder();
+            foreach (string s in Strings)
             {
                 sb.AppendLine(s);
             }
             return sb.ToString();
         }
+
+        #endregion
     }
 }
