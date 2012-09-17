d7849c46cf6bfae2380cc0f0ec891633e34e282d
egr <egoroff@gmail.com>
2012-09-17 14:49:13 +0400

    main window icon changed

--- logviewer/MainDlg.Designer.cs
+++ logviewer/MainDlg.Designer.cs
@@ -199,6 +199,7 @@
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainDlg";
             this.Text = "Log Viewer";
