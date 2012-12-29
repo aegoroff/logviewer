eaace902a3129468ee3a8402c9f8ee20f59f0f63
egr <egoroff@gmail.com>
2012-12-29 09:44:48 +0400

    LogMessage structure tests added

---
+++ logviewer.tests/TLogMessage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using logviewer.core;
+
+namespace logviewer.tests
+{
+    [TestFixture]
+    public class TLogMessage
+    {
+        [SetUp]
+        public void Setup()
+        {
+            this.m = new LogMessage(new List<string>());
+        }
+
+        private LogMessage m;
+        private const string H = "h";
+        private const string B = "b";
+
+        [Test]
+        public void Full()
+        {
+            this.m.AddLine(H);
+            this.m.AddLine(B);
+            Assert.That(this.m.IsEmpty, Is.False);
+            Assert.That(this.m.Header, Is.EqualTo(H));
+            Assert.That(this.m.Body, Is.EqualTo(B));
+        }
+
+        [Test]
+        public void FullToString()
+        {
+            this.m.AddLine(H);
+            this.m.AddLine(B);
+            Assert.That(this.m.ToString(), Is.EqualTo(H + Environment.NewLine + B));
+        }
+
+        [Test]
+        public void IsEmpty()
+        {
+            Assert.That(this.m.IsEmpty);
+            Assert.That(this.m.Body, Is.Empty);
+            Assert.That(this.m.Header, Is.Empty);
+        }
+
+        [Test]
+        public void OnlyHead()
+        {
+            this.m.AddLine(H);
+            Assert.That(this.m.IsEmpty, Is.False);
+            Assert.That(this.m.Header, Is.EqualTo(H));
+            Assert.That(this.m.Body, Is.Empty);
+        }
+
+        [Test]
+        public void OnlyHeadToString()
+        {
+            this.m.AddLine(H);
+            Assert.That(this.m.ToString(), Is.EqualTo(H));
+        }
+    }
+}
