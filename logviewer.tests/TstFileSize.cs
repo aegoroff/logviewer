d21590aa6d0160e1725b7991709f4a1aba4577f0
egoroff <egoroff@gmail.com>
2013-09-02 23:45:43 +0400

    FileSize to string testing

--- logviewer.tests/TstFileSize.cs
+++ logviewer.tests/TstFileSize.cs
@@ -6,21 +6,22 @@ namespace logviewer.tests
     [TestFixture]
     internal class TstFileSize
     {
-        [TestCase(0UL, SizeUnit.Bytes, 0.0)]
-        [TestCase(1023UL, SizeUnit.Bytes, 0.0)]
-        [TestCase(1024UL, SizeUnit.KBytes, 1.0)]
-        [TestCase(2UL * 1024UL, SizeUnit.KBytes, 2.0)]
-        [TestCase(2UL * 1024UL * 1024UL, SizeUnit.MBytes, 2.0)]
-        [TestCase(2UL * 1024UL * 1024UL * 1024UL, SizeUnit.GBytes, 2.0)]
-        [TestCase(2UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.TBytes, 2.0)]
-        [TestCase(2UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.PBytes, 2.0)]
-        [TestCase(2UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.EBytes, 2.0)]
-        public void Normalize(ulong size, SizeUnit unit, double value)
+        [TestCase(0UL, SizeUnit.Bytes, 0.0, "0 Bytes")]
+        [TestCase(1023UL, SizeUnit.Bytes, 0.0, "1023 Bytes")]
+        [TestCase(1024UL, SizeUnit.KBytes, 1.0, "1,00 Kb (1024 Bytes)")]
+        [TestCase(2UL * 1024UL, SizeUnit.KBytes, 2.0, "2,00 Kb (2048 Bytes)")]
+        [TestCase(2UL * 1024UL * 1024UL, SizeUnit.MBytes, 2.0, "2,00 Mb (2097152 Bytes)")]
+        [TestCase(2UL * 1024UL * 1024UL * 1024UL, SizeUnit.GBytes, 2.0, "2,00 Gb (2147483648 Bytes)")]
+        [TestCase(2UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.TBytes, 2.0, "2,00 Tb (2199023255552 Bytes)")]
+        [TestCase(2UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.PBytes, 2.0, "2,00 Pb (2251799813685248 Bytes)")]
+        [TestCase(2UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.EBytes, 2.0, "2,00 Eb (2305843009213693952 Bytes)")]
+        public void Normalize(ulong size, SizeUnit unit, double value, string str)
         {
             var sz = new FileSize(size);
             Assert.That(sz.Bytes, Is.EqualTo(size));
             Assert.That(sz.Unit, Is.EqualTo(unit));
             Assert.That(sz.Value, Is.EqualTo(value));
+            Assert.That(sz.ToString(), Is.EqualTo(str));
         }
     }
 }
