31f63720bda428e908649494e4bf4e77d54c5f68
egoroff <egoroff@gmail.com>
2013-09-06 23:41:56 +0400

    SQLite wrapper draft. SQLite ADO.NET provired added into setup package

---
+++ logviewer.core/LogStore.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace logviewer.core
+{
+    public sealed class LogStore : IDisposable
+    {
+        #region Constants and Fields
+
+        private readonly string databasePath;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public LogStore()
+        {
+            databasePath = Path.GetTempFileName();
+            SQLiteConnection.CreateFile(databasePath);
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public void AddMessage(LogMessage message)
+        {
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (File.Exists(databasePath))
+                {
+                    File.Delete(databasePath);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
