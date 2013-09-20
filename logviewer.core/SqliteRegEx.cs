3fe737ccf142dceda0f9059ce19a33916ef78c9c
egoroff <egoroff@gmail.com>
2013-09-20 22:20:21 +0400

    fixes #34 exception on invalid regexp handling and it's very fast now

--- logviewer.core/SqliteRegEx.cs
+++ logviewer.core/SqliteRegEx.cs
@@ -11,11 +11,26 @@ namespace logviewer.core
     [SQLiteFunction(Name = "REGEXP", Arguments = 2, FuncType = FunctionType.Scalar)]
     internal class SqliteRegEx : SQLiteFunction
     {
+        private static string invalid;
+
         public override object Invoke(object[] args)
         {
             var input = Convert.ToString(args[1]);
             var pattern = Convert.ToString(args[0]);
-            return Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            try
+            {
+                if (invalid != null && invalid.Equals(pattern))
+                {
+                    return false;
+                }
+                return Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            }
+            catch (Exception e)
+            {
+                Log.Instance.Info(e.Message, e);
+                invalid = pattern;
+            }
+            return false;
         }
     }
 }
