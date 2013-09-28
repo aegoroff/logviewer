f2d7b7a545fd9398b064c017d560290cf3558eb7
egoroff <egoroff@gmail.com>
2013-09-29 01:21:47 +0400

    messages counting implemented

--- rdr/rdr.c
+++ rdr/rdr.c
@@ -16,6 +16,8 @@
 #include "apr.h"
 #include "apr_pools.h"
 #include "apr_strings.h"
+#include "apr_file_io.h"
+#include "pcre.h"
 
 #ifdef WIN32
 #include "..\srclib\DebugHelplers.h"
@@ -27,9 +29,12 @@
 #define PATH_ELT_SEPARATOR '\\'
 #define NUMBER_PARAM_FMT_STRING "%lu"
 #define BIG_NUMBER_PARAM_FMT_STRING "%llu"
+#define START_MSG "^\\s*\\[?\\d{4}-\\d{2}-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2}([,.]\\d{3,})?.*"
 
 #define MAX_LINE_SIZE 32 * BINARY_THOUSAND - 1
+#define MAX_STRING_LEN 2048 * 2
 
+apr_pool_t* pcrePool = NULL;
 
 void PrintError(apr_status_t status)
 {
@@ -39,15 +44,28 @@ void PrintError(apr_status_t status)
     NewLine();
 }
 
+void* PcreAlloc(size_t size)
+{
+    return apr_palloc(pcrePool, size);
+}
+
 int main(int argc, const char* const argv[])
 {
     apr_pool_t* pool = NULL;
     apr_status_t status = APR_SUCCESS;
     int nerrors;
-    apr_off_t limitValue = 0;
-    apr_off_t offsetValue = 0;
 
-    struct arg_file *file          = arg_file0("f", "file", NULL, "full path file to calculate hash sum for");
+    pcre* re = NULL;
+    const char* error = NULL;
+    int erroffset = 0;
+    int rc = 0;
+    int flags  = PCRE_NOTEMPTY;
+
+    apr_file_t* fileHandle = NULL;
+    char* line = NULL;
+    long long messages = 0;
+
+    struct arg_file *file          = arg_file0("f", "file", NULL, "full path to log file");
     struct arg_lit  *help          = arg_lit0("h", "help", "print this help and exit");
     struct arg_end  *end           = arg_end(10);
 
@@ -61,6 +79,8 @@ int main(int argc, const char* const argv[])
 
     setlocale(LC_ALL, ".ACP");
     setlocale(LC_NUMERIC, "C");
+
+    pcre_malloc = PcreAlloc;
 
     status = apr_app_initialize(&argc, &argv, NULL);
     if (status != APR_SUCCESS) {
@@ -90,7 +110,41 @@ int main(int argc, const char* const argv[])
         goto cleanup;
     }
 
+    pcrePool = pool; // needed for pcre_alloc (PcreAlloc) function
 
+    re = pcre_compile(START_MSG,           /* the pattern */
+                      PCRE_UTF8,
+                      &error,          /* for error message */
+                      &erroffset,      /* for error offset */
+                      0);
+    if (!re) {
+        CrtPrintf("%s", error); //-V111
+        goto cleanup;
+    }
+
+    status = apr_file_open(&fileHandle, file->filename[0], APR_FOPEN_READ | APR_FOPEN_BUFFERED, APR_FPROT_WREAD, pool);
+    if (status != APR_SUCCESS) {
+        PrintError(status);
+        goto cleanup;
+    }
+    line = (char*)apr_pcalloc(pool, MAX_STRING_LEN);
+
+    while (status != APR_EOF) {
+        status = apr_file_gets(line, MAX_STRING_LEN, fileHandle);
+        rc = pcre_exec(
+            re,                   /* the compiled pattern */
+            0,                    /* no extra data - pattern was not studied */
+            line,                  /* the string to match */
+            (int)strlen(line),     /* the length of the string */
+            0,                    /* start at offset 0 in the subject */
+            flags,
+            NULL,              /* output vector for substring information */
+            0);           /* number of elements in the output vector */
+        if (rc >= 0) {
+            ++messages;
+        }
+    }
+    CrtPrintf(BIG_NUMBER_PARAM_FMT_STRING, messages);
 
 cleanup:
     /* deallocate each non-null entry in argtable[] */
