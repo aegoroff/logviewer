964267a154a317f8ea9c8794dbe6f4c77f7d85cd
egr <egoroff@gmail.com>
2013-10-17 12:16:12 +0400

    temp_store = MEMORY instead of temp_store = 2 to improve readability

--- logviewer.core/LogStore.cs
+++ logviewer.core/LogStore.cs
@@ -50,7 +50,7 @@ namespace logviewer.core
             const string SyncOff = @"PRAGMA synchronous = OFF;";
             const string Journal = @"PRAGMA journal_mode = OFF;";
             const string Cache = @"PRAGMA cache_size = {0};";
-            const string Temp = @"PRAGMA temp_store = 2;";
+            const string Temp = @"PRAGMA temp_store = MEMORY;";
             const string Encode = @"PRAGMA encoding = 'UTF-8';";
             const string Mmap = @"PRAGMA mmap_size={0};";
 
