0d741782983b6b15681a2c64a8ca6349e837fbe1
egoroff <egoroff@gmail.com>
2013-10-22 00:13:19 +0400

    shutdown timeout set so as not to hang infinitely

--- logviewer.core/ProducerConsumerQueue.cs
+++ logviewer.core/ProducerConsumerQueue.cs
@@ -43,7 +43,7 @@ namespace logviewer.core
             }
             foreach (var worker in this.workers)
             {
-                worker.Join();
+                worker.Join(TimeSpan.FromSeconds(EnqueueTimeoutMilliseconds * 1000));
             }
         }
 
