deb1622e1c25cc14f8f9f1be5b254c978d1f5b23
egoroff <egoroff@gmail.com>
2014-01-02 14:09:49 +0400

    refactoring: Rtf made auto property

--- logviewer.core/LogReadCompletedEventArgs.cs
+++ logviewer.core/LogReadCompletedEventArgs.cs
@@ -8,20 +8,15 @@ namespace logviewer.core
 {
     public class LogReadCompletedEventArgs : EventArgs
     {
-        private readonly string rtf;
-
         /// <summary>
         ///     Constructor
         /// </summary>
         /// <param name="rtf"></param>
         public LogReadCompletedEventArgs(string rtf)
         {
-            this.rtf = rtf;
+            this.Rtf = rtf;
         }
 
-        public string Rtf
-        {
-            get { return this.rtf; }
-        }
+        public string Rtf { get; private set; }
     }
 }
