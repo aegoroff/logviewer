48701df72b98525c7899440f07ae2712140acb03
egoroff <egoroff@gmail.com>
2014-03-19 22:00:30 +0400

    correct declension after numbers

--- logviewer.core/Extensions.cs
+++ logviewer.core/Extensions.cs
@@ -3,6 +3,8 @@
 // © 2012-2014 Alexander Egorov
 
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -70,17 +72,32 @@ namespace logviewer.core
 
         private static string WithHours(TimeSpan input)
         {
-            return input.Hours > 0 ? string.Format(Resources.RemainingWithHours, input.Hours, input.Minutes, input.Seconds) : null;
+            return input.Hours > 0 ? string.Format(Resources.RemainingWithHours, input.Hours, input.Minutes, input.Seconds, input.HoursToString(), input.MinutesToString(), input.SecondsToString()) : null;
         }
 
         private static string WithMinutes(TimeSpan input)
         {
-            return input.Minutes > 0 ? string.Format(Resources.RemainingWithMinutes, input.Minutes, input.Seconds) : null;
+            return input.Minutes > 0 ? string.Format(Resources.RemainingWithMinutes, input.Minutes, input.Seconds, input.MinutesToString(), input.SecondsToString()) : null;
         }
 
         private static string WithSeconds(TimeSpan input)
         {
-            return input.Seconds > 0 ? string.Format(Resources.RemainingOnlySeconds, input.Seconds) : null;
+            return input.Seconds > 0 ? string.Format(Resources.RemainingOnlySeconds, input.Seconds, input.SecondsToString()) : null;
+        }
+
+        private static string SecondsToString(this TimeSpan input)
+        {
+            return input.Seconds.Declension(Resources.SecondsNominative, Resources.SecondsGenitiveSingular, Resources.SecondsGenitivePlural);
+        }
+        
+        private static string MinutesToString(this TimeSpan input)
+        {
+            return input.Minutes.Declension(Resources.MinutesNominative, Resources.MinutesGenitiveSingular, Resources.MinutesGenitivePlural);
+        }
+        
+        private static string HoursToString(this TimeSpan input)
+        {
+            return input.Hours.Declension(Resources.HoursNominative, Resources.HoursGenitiveSingular, Resources.HoursGenitivePlural);
         }
 
         internal static string TimespanToHumanString(this TimeSpan timeSpan)
@@ -98,5 +115,59 @@ namespace logviewer.core
             }
             return Resources.RemainingLessThenSecond;
         }
+
+        private static readonly IDictionary<int, Func<int, string, string, string, string>> declensions = new Dictionary
+            <int, Func<int, string, string, string, string>>
+        {
+            { 1049, DeclensionRu }
+        };
+
+        private static string Declension(this int number, string nominative, string genitiveSingular,
+            string genitivePlural)
+        {
+            return declensions.ContainsKey(CultureInfo.CurrentCulture.LCID)
+                ? declensions[CultureInfo.CurrentCulture.LCID](number, nominative, genitiveSingular, genitivePlural)
+                : DeclensionEn(number, nominative, genitiveSingular, genitivePlural);
+        }
+
+        /// <summary>
+         /// Does a word declension after a number.
+         /// </summary>
+         /// <param name="number"></param>
+         /// <param name="nominative"></param>
+         /// <param name="genitiveSingular"></param>
+         /// <param name="genitivePlural"></param>
+         /// <returns></returns>
+         private static string DeclensionRu(int number, string nominative, string genitiveSingular, string genitivePlural)
+         {
+             var lastDigit = number % 10;
+             var lastTwoDigits = number % 100;
+             if (lastDigit == 1 && lastTwoDigits != 11)
+             {
+                 return nominative;
+             }
+             if (lastDigit == 2 && lastTwoDigits != 12 || lastDigit == 3 && lastTwoDigits != 13 || lastDigit == 4 && lastTwoDigits != 14)
+             {
+                 return genitiveSingular;
+             }
+             return genitivePlural;
+         }
+        
+        /// <summary>
+         /// Does a word declension after a number.
+         /// </summary>
+         /// <param name="number"></param>
+         /// <param name="nominative"></param>
+         /// <param name="genitiveSingular"></param>
+         /// <param name="genitivePlural"></param>
+         /// <returns></returns>
+         private static string DeclensionEn(int number, string nominative, string genitiveSingular, string genitivePlural)
+         {
+             if (number == 1 || number == -1)
+             {
+                 return nominative;
+             }
+             return genitiveSingular ?? genitivePlural;
+         }
     }
 }
