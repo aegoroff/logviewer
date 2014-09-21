7bcfa4acac982a24e8e6c62d14ab7fb81823e4bb
egoroff <egoroff@gmail.com>
2014-09-21 19:28:01 +0400

    copyright actualized

--- logviewer/Properties/AssemblyInfo.cs
+++ logviewer/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexander Egorov")]
 [assembly: AssemblyProduct("logviewer")]
-[assembly: AssemblyCopyright("Copyright ©  2012-2013")]
+[assembly: AssemblyCopyright("Copyright ©  2012-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
