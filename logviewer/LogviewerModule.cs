538e2c1e201845dd38e230d1cf2b2b6dcb46a274
egoroff <egoroff@gmail.com>
2014-10-03 22:19:01 +0400

    Obsolete settings controller constructor parameter removed

--- logviewer/LogviewerModule.cs
+++ logviewer/LogviewerModule.cs
@@ -2,9 +2,7 @@
 // Created at: 14.09.2013
 // © 2012-2014 Alexander Egorov
 
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using logviewer.core;
 using Ninject.Modules;
 
@@ -12,16 +10,6 @@ namespace logviewer
 {
     public class LogviewerModule : NinjectModule
     {
-        private static readonly string[] markers =
-        {
-            "TraceMarker",
-            "DebugMarker",
-            "InfoMarker",
-            "WarnMarker",
-            "ErrorMarker",
-            "FatalMarker"
-        };
-
         public override void Load()
         {
             this.Bind<ILogView>().To<MainDlg>();
@@ -29,18 +17,12 @@ namespace logviewer
             
             this.Bind<ISettingsProvider>().To<SqliteSettingsProvider>().InSingletonScope()
                 .WithConstructorArgument("defaultStartMessageTemplate", ConfigurationManager.AppSettings["StartMessagePattern"])
-                .WithConstructorArgument("defaultLeveles", Levels)
                 .WithConstructorArgument("settingsDatabaseFileName",
                     ConfigurationManager.AppSettings["SettingsDatabase"])
                 .WithConstructorArgument("defaultKeepLastNFiles", 10)
                 .WithConstructorArgument("defaultPageSize", 2000);
             
             
-        }
-
-        private static IEnumerable<string> Levels
-        {
-            get { return markers.Select(t => ConfigurationManager.AppSettings[t]); }
         }
     }
 }
