a1b0041830775043b64e0fb29038864854bdf291
egoroff <egoroff@gmail.com>
2014-10-04 12:36:11 +0400

    template compilation speedup

--- logviewer.core/GrokMatcher.cs
+++ logviewer.core/GrokMatcher.cs
@@ -6,21 +6,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Atn;
+using Antlr4.Runtime.Misc;
 
 namespace logviewer.core
 {
     public class GrokMatcher
     {
-        private readonly Regex regex;
+        private Regex regex;
         private readonly List<Semantic> semantics = new List<Semantic>();
 
         public GrokMatcher(string grok, RegexOptions options = RegexOptions.None)
         {
+            this.Compile(grok, options);
+        }
+
+        private void Compile(string grok, RegexOptions options)
+        {
             ICharStream inputStream = new AntlrInputStream(grok);
             var lexer = new GrokLexer(inputStream);
             var tokenStream = new CommonTokenStream(lexer);
-            var parser = new GrokParser(tokenStream);
-            var tree = parser.parse();
+            var parser = new GrokParser(tokenStream)
+            {
+                Interpreter = { PredictionMode = PredictionMode.Sll }
+            };
+            parser.RemoveErrorListeners();
+            parser.ErrorHandler = new BailErrorStrategy();
+
+            var tree = TryCompile(parser, tokenStream);
 
             if (parser.NumberOfSyntaxErrors > 0)
             {
@@ -34,6 +47,24 @@ namespace logviewer.core
                 this.semantics.AddRange(grokVisitor.Semantics);
             }
             this.regex = new Regex(this.Template, options);
+        }
+
+        private static GrokParser.ParseContext TryCompile(GrokParser parser, BufferedTokenStream tokens)
+        {
+            try
+            {
+                return parser.parse();
+            }
+            catch (ParseCanceledException e)
+            {
+                tokens.Reset();
+                parser.Reset();
+                Log.Instance.Debug(e);
+                parser.ErrorListeners.Add(ConsoleErrorListener<IToken>.Instance);
+                parser.ErrorHandler = new DefaultErrorStrategy();
+                parser.Interpreter.PredictionMode = PredictionMode.Ll;
+                return parser.parse();
+            }
         }
 
         public string Template { get; private set; }
