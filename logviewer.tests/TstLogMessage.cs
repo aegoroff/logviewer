3cd7c23b3cb64c5b5c2fac4c6c44536f93925ef8
egr <egoroff@gmail.com>
2014-10-24 12:03:49 +0400

    Parsing log level tests added

--- logviewer.tests/TstLogMessage.cs
+++ logviewer.tests/TstLogMessage.cs
@@ -2,6 +2,8 @@
 // Created at: 02.01.2013
 // © 2012-2014 Alexander Egorov
 
+using System.Collections;
+using System.Collections.Generic;
 using logviewer.core;
 using NUnit.Framework;
 
@@ -77,6 +79,36 @@ namespace logviewer.tests
             Assert.That(this.m.IsEmpty, Is.False);
             Assert.That(this.m.Header, Is.EqualTo(H));
             Assert.That(this.m.Body, Is.Empty);
+        }
+        
+        [TestCase("Trace", LogLevel.Trace)]
+        [TestCase("TRACE", LogLevel.Trace)]
+        [TestCase("Debug", LogLevel.Debug)]
+        [TestCase("DEBUG", LogLevel.Debug)]
+        [TestCase("Info", LogLevel.Info)]
+        [TestCase("INFO", LogLevel.Info)]
+        [TestCase("Warn", LogLevel.Warn)]
+        [TestCase("WARN", LogLevel.Warn)]
+        [TestCase("Warning", LogLevel.Trace)]
+        [TestCase("Error", LogLevel.Error)]
+        [TestCase("ERROR", LogLevel.Error)]
+        [TestCase("Fatal", LogLevel.Fatal)]
+        [TestCase("FATAL", LogLevel.Fatal)]
+        public void ParseLogLevel(string input, LogLevel result)
+        {
+            this.m.AddLine(H);
+            SemanticProperty s = new SemanticProperty("level", ParserType.LogLevel);
+            Rule r = new Rule("LogLevel");
+            ISet<Rule> rules = new HashSet<Rule>();
+            rules.Add(r);
+            IDictionary<SemanticProperty, ISet<Rule>> schema = new Dictionary<SemanticProperty, ISet<Rule>>();
+            schema.Add(s, rules);
+            IDictionary<string, string> props = new Dictionary<string, string>();
+            props.Add(s.Name, input);
+            this.m.AddProperties(props);
+            this.m.Cache(schema);
+
+            Assert.That((LogLevel)this.m.IntegerProperty(s.Name), Is.EqualTo(result));
         }
     }
 }
