b84388564fc395f52467b54faeb5ff18034ce129
egr <egoroff@gmail.com>
2014-11-19 12:03:43 +0300

    build warning fixed

--- logviewer.engine/Properties/AssemblyInfo.cs
+++ logviewer.engine/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -20,6 +21,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("45cf0211-8153-4898-be29-0802d32eacd7")]
