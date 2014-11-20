757d047c3f926cd25e47d67744b1670fd0ff890d
egr <egoroff@gmail.com>
2014-11-20 13:31:15 +0300

    install script run fixed

--- logviewer.engine/Install.ps1
+++ logviewer.engine/Install.ps1
@@ -1,40 +1,26 @@
 param($installPath, $toolsPath, $package, $project)
 
-Edit-XmlNodes -project $project -name "grok.patterns"
-Edit-XmlNodes -project $project -name "webservers.patterns"
+$Global:project = $project
 
-function Edit-XmlNodes {
-param (
-    $project = $(throw "project is a required parameter"),
-    $name = $(throw "name is a required parameter")
-)   
-    # Load project XML.
-    $doc = New-Object System.Xml.XmlDocument
-    $doc.Load($project.FullName)
-    $namespace = 'http://schemas.microsoft.com/developer/msbuild/2003'
+Write-Host (Join-Path $installPath "content")
 
-    # Find the node containing the file. The tag "Content" may be replace by "None" depending of the case, check your .csproj file.    
-    $xmlHode = Select-Xml "//msb:Project/msb:ItemGroup/msb:None[@Include='$name']" $doc -Namespace @{msb = $namespace}
+
+function Edit-ProjectItem($name) {
+    Write-Host "Found pattern: " $name
     
-    if(!$xmlHode) {
-        $xmlHode = Select-Xml "//msb:Project/msb:ItemGroup/msb:Content[@Include='$name']" $doc -Namespace @{msb = $namespace}
-    }
-    
-    #check if the node exists.
-    if($xmlHode -ne $null)
-    {
-        $nodeName = "CopyToOutputDirectory"
+    $configItem = $project.ProjectItems.Item($name)
 
-        #Check if the property already exists, just in case.
-        $property = $xmlHode.Node.SelectSingleNode($nodeName)
-        if($property -eq $null)
-        {
-            $property = $doc.CreateElement($nodeName, $namespace)
-            $property.AppendChild($doc.CreateTextNode("Always"))
-            $xmlHode.Node.AppendChild($property)
+    # set 'Copy To Output Directory' to 'Always'
+    $copyToOutput = $configItem.Properties.Item("CopyToOutputDirectory")
+    $copyToOutput.Value = 1
 
-            # Save changes.
-            $doc.Save($project.FullName)
-        }
-    }
+    # set 'Build Action' to 'None'
+    $buildAction = $configItem.Properties.Item("BuildAction")
+    $buildAction.Value = 0
+}
+
+Get-ChildItem (Join-Path $installPath "content") -Filter *.patterns | `
+Foreach-Object {
+    $name = $_.Name
+    Edit-ProjectItem $name
 }
