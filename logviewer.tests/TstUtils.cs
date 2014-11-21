d69b68299a3fda55df17c3ebc554f5ccf93e393a
egoroff <egoroff@gmail.com>
2014-11-22 00:06:09 +0300

    culture specific tests fixed

--- logviewer.tests/TstUtils.cs
+++ logviewer.tests/TstUtils.cs
@@ -53,16 +53,16 @@ namespace logviewer.tests
         [Theory]
         [InlineData(0UL, SizeUnit.Bytes, 0.0, "0 \\w+", false)]
         [InlineData(1023UL, SizeUnit.Bytes, 0.0, "1023 \\w+", false)]
-        [InlineData(1024UL, SizeUnit.KBytes, 1.0, "1,00 \\w{2} \\(1 024 \\w+\\)", false)]
-        [InlineData(2UL * 1024UL, SizeUnit.KBytes, 2.0, "2,00 \\w{2} \\(2 048 \\w+\\)", false)]
-        [InlineData(2UL * 1024UL * 1024UL, SizeUnit.MBytes, 2.0, "2,00 \\w{2} \\(2 097 152 \\w+\\)", false)]
-        [InlineData(2UL * 1024UL * 1024UL * 1024UL, SizeUnit.GBytes, 2.0, "2,00 \\w{2} \\(2 147 483 648 \\w+\\)", false)]
-        [InlineData(2UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.TBytes, 2.0, "2,00 \\w{2} \\(2 199 023 255 552 \\w+\\)", false)]
+        [InlineData(1024UL, SizeUnit.KBytes, 1.0, "1[.,]00 \\w{2} \\(1 024 \\w+\\)", false)]
+        [InlineData(2UL * 1024UL, SizeUnit.KBytes, 2.0, "2[.,]00 \\w{2} \\(2 048 \\w+\\)", false)]
+        [InlineData(2UL * 1024UL * 1024UL, SizeUnit.MBytes, 2.0, "2[.,]00 \\w{2} \\(2 097 152 \\w+\\)", false)]
+        [InlineData(2UL * 1024UL * 1024UL * 1024UL, SizeUnit.GBytes, 2.0, "2[.,]00 \\w{2} \\(2 147 483 648 \\w+\\)", false)]
+        [InlineData(2UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.TBytes, 2.0, "2[.,]00 \\w{2} \\(2 199 023 255 552 \\w+\\)", false)]
         [InlineData(2UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.PBytes, 2.0,
-            "2,00 \\w{2} \\(2 251 799 813 685 248 \\w+\\)", false)]
+            "2[.,]00 \\w{2} \\(2 251 799 813 685 248 \\w+\\)", false)]
         [InlineData(2UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL, SizeUnit.EBytes, 2.0,
-            "2,00 \\w{2} \\(2 305 843 009 213 693 952 \\w+\\)", false)]
-        [InlineData(2UL * 1024UL, SizeUnit.KBytes, 2.0, "2,00 \\w{2}", true)]
+            "2[.,]00 \\w{2} \\(2 305 843 009 213 693 952 \\w+\\)", false)]
+        [InlineData(2UL * 1024UL, SizeUnit.KBytes, 2.0, "2[.,]00 \\w{2}", true)]
         public void TestFileSizeNormalize(ulong size, SizeUnit unit, double value, string str, bool bigWithoutBytes)
         {
             var sz = new FileSize(size, bigWithoutBytes);
