9980911709c2bf52f3c9517091de580fd85c4a1b
egoroff <egoroff@gmail.com>
2014-11-22 19:18:30 +0300

    wix replacement rewritten

--- wix.ps1
+++ wix.ps1
@@ -4,7 +4,7 @@ $pattern = 'define Version = "(\d+)\.(\d+)\.(\d+).(\d+)"'
 $replacement = 'define Version = "' + $ver + '"'
 
 foreach ($file in $files) {
-	(Get-Content $file) | `
-	Foreach-Object {$_ -replace $pattern, $replacement} | `
-	Set-Content $file
+	$content = (Get-Content $file)
+	$content = $content -replace $pattern, $replacement
+	$content | Out-File $file
 }
