3960871dce1251f339f0ef8eb1189f40a2cea40c
egoroff <egoroff@gmail.com>
2014-11-22 20:28:00 +0300

    one more try to wix replacement

--- wix.ps1
+++ wix.ps1
@@ -1,10 +1,7 @@
-param($files, $ver)
+param($file, $ver)
 
 $pattern = 'define Version = "(\d+)\.(\d+)\.(\d+).(\d+)"'
 $replacement = 'define Version = "' + $ver + '"'
-
-foreach ($file in $files) {
-	$content = (Get-Content $file)
-	$content = $content -replace $pattern, $replacement
-	$content | Out-File $file
-}
+$content = (Get-Content $file)
+$content = $content -replace $pattern, $replacement
+$content | Out-File $file
