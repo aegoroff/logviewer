d498453cbad520be2fdbb76a1164c75550864056
egoroff <egoroff@gmail.com>
2014-11-22 21:04:59 +0300

    WiX version replacement using msbuild

--- wix.ps1
+++
@@ -1,7 +0,0 @@
-param($file, $ver)
-
-$pattern = 'define Version = "(\d+)\.(\d+)\.(\d+).(\d+)"'
-$replacement = 'define Version = "' + $ver + '"'
-$content = (Get-Content $file)
-$content = $content -replace $pattern, $replacement
-$content | Out-File $file
