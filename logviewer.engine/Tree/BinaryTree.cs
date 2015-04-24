57304024ddbcd54143cf1aaee4a5289e0b696dfe
egoroff <egoroff@gmail.com>
2015-04-25 00:00:04 +0300

    Tree traverse methods added

--- logviewer.engine/Tree/BinaryTree.cs
+++ logviewer.engine/Tree/BinaryTree.cs
@@ -1,4 +1,6 @@
-﻿namespace logviewer.engine.Tree
+﻿using System;
+
+namespace logviewer.engine.Tree
 {
     /// <summary>
     /// Represents binary tree abstraction
@@ -25,6 +27,83 @@
         public virtual void Clear()
         {
             this.Root = null;
+        }
+
+        /// <summary>
+        /// Preorder full tree traverse
+        /// </summary>
+        /// <param name="action">Action to execute for each node</param>
+        public void PreorderTraversal(Action<BinaryTreeNode<T>> action)
+        {
+            this.PreorderTraversal(this.Root, action);
+        }
+        
+        /// <summary>
+        /// Preorder custom tree traverse
+        /// </summary>
+        /// <param name="current">Node to start travese from</param>
+        /// <param name="action">Action to execute for each node</param>
+        public void PreorderTraversal(BinaryTreeNode<T> current, Action<BinaryTreeNode<T>> action)
+        {
+            if (current == null)
+            {
+                return;
+            }
+            action(current);
+            this.PreorderTraversal(current.Left, action);
+            this.PreorderTraversal(current.Right, action);
+        }
+
+        /// <summary>
+        /// Inorder full tree traverse
+        /// </summary>
+        /// <param name="action">Action to execute for each node</param>
+        public void InorderTraversal(Action<BinaryTreeNode<T>> action)
+        {
+
+            this.InorderTraversal(Root, action);
+        }
+        
+        /// <summary>
+        /// Inorder custom tree traverse
+        /// </summary>
+        /// <param name="current">Node to start travese from</param>
+        /// <param name="action">Action to execute for each node</param>
+        public void InorderTraversal(BinaryTreeNode<T> current, Action<BinaryTreeNode<T>> action)
+        {
+            if (current == null)
+            {
+                return;
+            }
+            this.InorderTraversal(current.Left, action);
+            action(current);
+            this.InorderTraversal(current.Right, action);
+        }
+        
+        /// <summary>
+        /// Postorder full tree traverse
+        /// </summary>
+        /// <param name="action">Action to execute for each node</param>
+        public void PostorderTraversal(Action<BinaryTreeNode<T>> action)
+        {
+
+            this.PostorderTraversal(Root, action);
+        }
+        
+        /// <summary>
+        /// Postorder custom tree traverse
+        /// </summary>
+        /// <param name="current">Node to start travese from</param>
+        /// <param name="action">Action to execute for each node</param>
+        public void PostorderTraversal(BinaryTreeNode<T> current, Action<BinaryTreeNode<T>> action)
+        {
+            if (current == null)
+            {
+                return;
+            }
+            this.PostorderTraversal(current.Left, action);
+            this.PostorderTraversal(current.Right, action);
+            action(current);
         }
     }
 }
--- logviewer.tests/TstBinaryTree.cs
+++ logviewer.tests/TstBinaryTree.cs
@@ -1,4 +1,5 @@
-﻿using logviewer.engine.Tree;
+﻿using System.Collections.Generic;
+using logviewer.engine.Tree;
 using Xunit;
 
 namespace logviewer.tests
@@ -23,6 +24,63 @@ namespace logviewer.tests
             Assert.Equal(1, tree.Root.Value);
             Assert.Equal(2, tree.Root.Left.Value);
             Assert.Equal(3, tree.Root.Right.Value);
+        }
+        
+        [Fact]
+        public void PreorderTraverse()
+        {
+            var tree = new BinaryTree<int>
+            {
+                Root = new BinaryTreeNode<int>(1)
+                {
+                    Left = new BinaryTreeNode<int>(2),
+                    Right = new BinaryTreeNode<int>(3)
+                }
+            };
+            var traverseResult = new List<int>();
+            tree.PreorderTraversal(node => traverseResult.Add(node.Value));
+            Assert.Equal(3, traverseResult.Count);
+            Assert.Equal(1, traverseResult[0]);
+            Assert.Equal(2, traverseResult[1]);
+            Assert.Equal(3, traverseResult[2]);
+        }
+        
+        [Fact]
+        public void InorderTraverse()
+        {
+            var tree = new BinaryTree<int>
+            {
+                Root = new BinaryTreeNode<int>(1)
+                {
+                    Left = new BinaryTreeNode<int>(2),
+                    Right = new BinaryTreeNode<int>(3)
+                }
+            };
+            var traverseResult = new List<int>();
+            tree.InorderTraversal(node => traverseResult.Add(node.Value));
+            Assert.Equal(3, traverseResult.Count);
+            Assert.Equal(2, traverseResult[0]);
+            Assert.Equal(1, traverseResult[1]);
+            Assert.Equal(3, traverseResult[2]);
+        }
+        
+        [Fact]
+        public void PostorderTraverse()
+        {
+            var tree = new BinaryTree<int>
+            {
+                Root = new BinaryTreeNode<int>(1)
+                {
+                    Left = new BinaryTreeNode<int>(2),
+                    Right = new BinaryTreeNode<int>(3)
+                }
+            };
+            var traverseResult = new List<int>();
+            tree.PostorderTraversal(node => traverseResult.Add(node.Value));
+            Assert.Equal(3, traverseResult.Count);
+            Assert.Equal(2, traverseResult[0]);
+            Assert.Equal(3, traverseResult[1]);
+            Assert.Equal(1, traverseResult[2]);
         }
     }
 }
