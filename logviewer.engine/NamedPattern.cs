1bb013919fc7729b20454da480e9c1218965d2f4
egoroff <egoroff@gmail.com>
2015-04-25 11:59:15 +0300

    property accessor added

--- logviewer.engine/NamedPattern.cs
+++ logviewer.engine/NamedPattern.cs
@@ -9,9 +9,12 @@ namespace logviewer.engine
     [DebuggerDisplay("{Content}")]
     internal class NamedPattern : Pattern
     {
+        internal string Property { get; private set; }
+
         internal NamedPattern(string property, string content)
             : base(string.Format(@"(?<{0}>{1})", property, content))
         {
+            this.Property = property;
         }
     }
 }
