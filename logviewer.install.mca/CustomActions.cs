6ad005c4c7bd4403197dbcccc4402899ce079770
egoroff <egoroff@gmail.com>
2015-05-29 21:55:58 +0300

    Custom action project added

---
+++ logviewer.install.mca/CustomActions.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace logviewer.install.mca
+{
+    public class CustomActions
+    {
+        private const string FakePrefix = "x";
+		private const string AppConfig = "logviewer.exe.config";
+        private const string Product = "logviewer";
+
+        private static readonly Dictionary<string, string> tempToConfigMap = new Dictionary<string, string>
+            {
+                { TempAppConfigFile, Path.Combine(AppPath, AppConfig) }
+            };
+
+        private static string TempAppConfigFile
+        {
+            get { return Path.Combine(Path.GetTempPath(), AppConfig); }
+        }
+
+        private static string AppPath
+        {
+            get { return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), Product); }
+        }
+        
+        [CustomAction]
+        public static ActionResult KeepConfigurationFiles(Session session)
+        {
+            KeepOldConfig(session, TempAppConfigFile);
+
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult RestoreConfigurationFiles(Session session)
+        {
+            RestoreConfig(session, TempAppConfigFile, RestoreAppConfig);
+            return ActionResult.Success;
+        }
+
+        private static void KeepOldConfig(Session session, string temp)
+        {
+            KeepOldConfig(session, tempToConfigMap[temp], temp);
+        }
+        
+        private static void KeepOldConfig(Session session, string source, string temp)
+        {
+            try
+            {
+                File.Copy(source, temp, true);
+                session.Log("keep config {1} to: {0}", temp, source);
+            }
+            catch (Exception e)
+            {
+                session.Log(e.ToString());
+            }
+        }
+
+        private static void RestoreConfig(Session session, string keptFile, RestoreConfigMethod restoreAction)
+        {
+            RestoreConfig(session, keptFile, tempToConfigMap[keptFile], restoreAction);
+        }
+        
+        private static void RestoreConfig(Session session, string keptFile, string targetFile, RestoreConfigMethod restoreAction)
+        {
+            if (!File.Exists(keptFile))
+            {
+                session.Log("No file stored before: {0}", keptFile);
+                return;
+            }
+            try
+            {
+                session.Log("Restoring: {0}", keptFile);
+
+                var src = new XmlDocument();
+                src.Load(keptFile);
+                
+                var tgt = new XmlDocument();
+                var tgtPath = targetFile;
+                tgt.Load(tgtPath);
+                
+                restoreAction(session, src, tgt);
+
+                tgt.Save(tgtPath);
+                session.Log("Result saved to: {0}", tgtPath);
+            }
+            catch (Exception e)
+            {
+                session.Log(e.ToString());
+            }
+            finally
+            {
+                CreateBackup(session, keptFile, targetFile);
+                File.Delete(keptFile);
+            }
+            session.Log("{0} restored", keptFile);
+        }
+
+        private static void CreateBackup(Session session, string keptFile, string targetFile)
+        {
+            try
+            {
+                File.Copy(keptFile, targetFile + ".bak", true);
+            }
+            catch (Exception e)
+            {
+                session.Log(e.ToString());
+            }
+        }
+
+        private static void RestoreAppConfig(Session session, XmlDocument src, XmlDocument tgt)
+        {
+            SaveMultipleNodes(session, src, tgt, "/configuration/appSettings/add", "key", "value");
+        }
+
+        private static void SaveMultipleNodes(Session session, XmlDocument src, XmlDocument tgt, string root, string keyAttr, string valueAttr, string namespaceUri = null)
+        {
+            XmlNodeList srcNodes;
+            if (namespaceUri != null)
+            {
+                var nsMgr = new XmlNamespaceManager(src.NameTable);
+                nsMgr.AddNamespace(FakePrefix, namespaceUri);
+                srcNodes = src.SelectNodes(root, nsMgr);
+            }
+            else
+            {
+                srcNodes = src.SelectNodes(root);
+            }
+            if (srcNodes == null)
+            {
+                session.Log("No data found in source {0}", src.BaseURI);
+                return;
+            }
+
+            var srcDict =
+                srcNodes.Cast<XmlNode>()
+                        .Where(setting => setting.Attributes != null && setting.Attributes[keyAttr] != null && setting.Attributes[valueAttr] != null)
+                        .ToDictionary(setting => setting.Attributes[keyAttr].Value,
+                                      setting => setting.Attributes[valueAttr].Value);
+
+            XmlNodeList tgtNodes;
+            if (namespaceUri != null)
+            {
+                var nsMgr = new XmlNamespaceManager(tgt.NameTable);
+                nsMgr.AddNamespace(FakePrefix, namespaceUri);
+                tgtNodes = tgt.SelectNodes(root, nsMgr);
+            }
+            else
+            {
+                tgtNodes = tgt.SelectNodes(root);
+            }
+
+            if (tgtNodes == null)
+            {
+                session.Log("No data found in target {0}", tgt.BaseURI);
+                return;
+            }
+
+            foreach (
+                var node in
+                    from XmlNode setting in tgtNodes
+                    where setting.Attributes != null && setting.Attributes[keyAttr] != null && setting.Attributes[valueAttr] != null
+                    where srcDict.ContainsKey(setting.Attributes[keyAttr].Value)
+                    select setting)
+            {
+                node.Attributes[valueAttr].Value = srcDict[node.Attributes[keyAttr].Value];
+                session.Log("Node {0} set to {1}", node.Attributes[keyAttr].Value, node.Attributes[valueAttr].Value);
+            }
+        }
+
+        private delegate void RestoreConfigMethod(Session session, XmlDocument src, XmlDocument tgt);
+    }
+}
---
+++ logviewer.install.mca/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("logviewer.install.mca")]
+[assembly: AssemblyDescription("A simple application for convenient working with application text logs.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alexander Egorov")]
+[assembly: AssemblyProduct("logviewer")]
+[assembly: AssemblyCopyright("Copyright ©  2012-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("DED97BA4-065B-43C4-82BC-B3E6FDD3A7E5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
