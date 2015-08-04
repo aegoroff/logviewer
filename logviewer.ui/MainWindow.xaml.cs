f4f646dedca722d5dd80cea36cfc8038db6ba744
egoroff <egoroff@gmail.com>
2015-08-05 00:57:36 +0300

    Migration to Fluent.Ribbon

--- logviewer.ui/MainWindow.xaml.cs
+++ logviewer.ui/MainWindow.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,13 +11,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Fluent;
 
 namespace logviewer.ui
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
