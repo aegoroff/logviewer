48bdbe5cd0b027619e8d64b2221811dd9b886f7c
egoroff <egoroff@gmail.com>
2015-08-05 00:01:16 +0300

    Ribbon experiments

--- logviewer.ui/MainWindow.xaml.cs
+++ logviewer.ui/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -18,7 +19,7 @@ namespace logviewer.ui
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
