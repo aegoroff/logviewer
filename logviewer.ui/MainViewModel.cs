5120482ace7df4462c6d076ae1fe6d3ed578e08d
egoroff <egoroff@gmail.com>
2015-09-12 14:01:25 +0300

    Parsing templates from setting displaying in UI

--- logviewer.ui/MainViewModel.cs
+++ logviewer.ui/MainViewModel.cs
@@ -3,6 +3,9 @@
 // © 2012-2015 Alexander Egorov
 
 using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using logviewer.core;
 using logviewer.ui.Properties;
 
 namespace logviewer.ui
@@ -10,6 +13,15 @@ namespace logviewer.ui
     public class MainViewModel
     {
         public static MainViewModel Current { get; } = new MainViewModel();
+
+        private readonly SqliteSettingsProvider settingsProvider = new SqliteSettingsProvider(ConfigurationManager.AppSettings["SettingsDatabase"], 10, 2000);
+
+        private MainViewModel()
+        {
+            this.Templates = new List<string>();
+            var fromDb = this.settingsProvider.ReadAllParsingTemplates();
+            this.Templates.AddRange(fromDb.Select(t => t.DisplayName));
+        }
 
         public List<string> MinLevelLabeles { get; private set; } = new List<string>
         {
@@ -30,5 +42,7 @@ namespace logviewer.ui
             Resources.ErrorLabel,
             Resources.FatalLabel
         };
+
+        public List<string> Templates { get; }
     }
 }
