b7b8ad1e2c9dc263901c1d8705bdeffeaa1b85dc
egoroff <egoroff@gmail.com>
2015-11-07 18:13:42 +0300

    WPF UI unhandled exception handler added.

--- logviewer.ui/App.xaml.cs
+++ logviewer.ui/App.xaml.cs
@@ -2,9 +2,12 @@
 // Created at: 04.08.2015
 // © 2012-2015 Alexander Egorov
 
+using System.Configuration;
 using System.Globalization;
 using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
+using logviewer.core;
 
 namespace logviewer.ui
 {
@@ -16,6 +19,17 @@ namespace logviewer.ui
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
             Thread.CurrentThread.CurrentUICulture = CultureInfo.CurrentCulture;
+            this.DispatcherUnhandledException += OnDispatcherUnhandledException;
+        }
+
+        private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var defectSender = new DefectSender(
+                ConfigurationManager.AppSettings["GitHubAccount"],
+                ConfigurationManager.AppSettings["GitHubProject"]);
+            var task = defectSender.Send(e.Exception);
+            MiniDump.CreateMiniDump();
+            task.Wait();
         }
     }
 }
