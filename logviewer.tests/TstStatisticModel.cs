e01957736267af2de0df2153eda95eba41a320a7
egoroff <egoroff@gmail.com>
2016-08-14 11:48:25 +0300

    Define sync context. Common logic moved to ctor

--- logviewer.tests/TstStatisticModel.cs
+++ logviewer.tests/TstStatisticModel.cs
@@ -17,93 +17,93 @@ namespace logviewer.tests
     public class TstStatisticModel
     {
         private const int DelayMilliseconds = 1000;
+        private readonly Mock<ILogStore> store;
+        private readonly StatisticModel model;
+
+        public TstStatisticModel()
+        {
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+            this.store = new Mock<ILogStore>();
+            this.model = new StatisticModel(this.store.Object, "1", "ru");
+        }
 
         [Fact]
         public void LoadStatistic_EmptyStore_AllLevelsCountZero()
         {
             // Arrange
-            var store = new Mock<ILogStore>();
-            StatisticModel model = new StatisticModel(store.Object, "1", "ru");
-
-            store.Setup(
+            this.store.Setup(
                 x => x.CountMessages(It.IsAny<LogLevel>(), It.IsAny<LogLevel>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(0);
 
-            store.Setup(
+            this.store.Setup(
                 x => x.SelectDateUsingFunc("min", LogLevel.Trace, LogLevel.Fatal, null, false)).Returns(DateTime.MinValue);
 
-            store.Setup(
+            this.store.Setup(
                 x => x.SelectDateUsingFunc("max", LogLevel.Trace, LogLevel.Fatal, null, false)).Returns(DateTime.MinValue);
 
-            store.SetupGet(x => x.DatabasePath).Returns(string.Empty);
+            this.store.SetupGet(x => x.DatabasePath).Returns(string.Empty);
 
             // Act
-            model.LoadStatistic();
+            this.model.LoadStatistic();
 
             // Assert
-            SpinWait.SpinUntil(() => model.Items.Count == 10, TimeSpan.FromMilliseconds(DelayMilliseconds));
-            model.Items.Single(x => x.Key == LogLevel.Trace.ToString()).Value.Should().Be("0");
-            model.Items.Single(x => x.Key == LogLevel.Debug.ToString()).Value.Should().Be("0");
-            model.Items.Single(x => x.Key == LogLevel.Info.ToString()).Value.Should().Be("0");
-            model.Items.Single(x => x.Key == "Warning").Value.Should().Be("0");
-            model.Items.Single(x => x.Key == LogLevel.Error.ToString()).Value.Should().Be("0");
-            model.Items.Single(x => x.Key == LogLevel.Fatal.ToString()).Value.Should().Be("0");
+            SpinWait.SpinUntil(() => this.model.Items.Count == 10, TimeSpan.FromMilliseconds(DelayMilliseconds));
+            this.model.Items.Single(x => x.Key == LogLevel.Trace.ToString()).Value.Should().Be("0");
+            this.model.Items.Single(x => x.Key == LogLevel.Debug.ToString()).Value.Should().Be("0");
+            this.model.Items.Single(x => x.Key == LogLevel.Info.ToString()).Value.Should().Be("0");
+            this.model.Items.Single(x => x.Key == "Warning").Value.Should().Be("0");
+            this.model.Items.Single(x => x.Key == LogLevel.Error.ToString()).Value.Should().Be("0");
+            this.model.Items.Single(x => x.Key == LogLevel.Fatal.ToString()).Value.Should().Be("0");
         }
 
         [Fact]
         public void LoadStatistic_OnlyTrace_TraceReturnNotZero()
         {
             // Arrange
-            var store = new Mock<ILogStore>();
-            StatisticModel model = new StatisticModel(store.Object, "1", "ru");
+            this.store.Setup(x => x.CountMessages(LogLevel.Trace, LogLevel.Trace, null, false, true)).Returns(1);
+            this.store.Setup(x => x.CountMessages(LogLevel.Debug, LogLevel.Debug, null, false, true)).Returns(0);
+            this.store.Setup(x => x.CountMessages(LogLevel.Info, LogLevel.Info, null, false, true)).Returns(0);
+            this.store.Setup(x => x.CountMessages(LogLevel.Warn, LogLevel.Warn, null, false, true)).Returns(0);
+            this.store.Setup(x => x.CountMessages(LogLevel.Error, LogLevel.Error, null, false, true)).Returns(0);
+            this.store.Setup(x => x.CountMessages(LogLevel.Fatal, LogLevel.Fatal, null, false, true)).Returns(0);
 
-            store.Setup(x => x.CountMessages(LogLevel.Trace, LogLevel.Trace, null, false, true)).Returns(1);
-            store.Setup(x => x.CountMessages(LogLevel.Debug, LogLevel.Debug, null, false, true)).Returns(0);
-            store.Setup(x => x.CountMessages(LogLevel.Info, LogLevel.Info, null, false, true)).Returns(0);
-            store.Setup(x => x.CountMessages(LogLevel.Warn, LogLevel.Warn, null, false, true)).Returns(0);
-            store.Setup(x => x.CountMessages(LogLevel.Error, LogLevel.Error, null, false, true)).Returns(0);
-            store.Setup(x => x.CountMessages(LogLevel.Fatal, LogLevel.Fatal, null, false, true)).Returns(0);
-
-            store.Setup(
+            this.store.Setup(
                 x => x.SelectDateUsingFunc("min", LogLevel.Trace, LogLevel.Fatal, null, false)).Returns(DateTime.MinValue);
 
-            store.Setup(
+            this.store.Setup(
                 x => x.SelectDateUsingFunc("max", LogLevel.Trace, LogLevel.Fatal, null, false)).Returns(DateTime.MinValue);
 
-            store.SetupGet(x => x.DatabasePath).Returns(string.Empty);
+            this.store.SetupGet(x => x.DatabasePath).Returns(string.Empty);
 
             // Act
-            model.LoadStatistic();
+            this.model.LoadStatistic();
 
             // Assert
-            SpinWait.SpinUntil(() => model.Items.Count == 10, TimeSpan.FromMilliseconds(DelayMilliseconds));
-            model.Items.Single(x => x.Key == LogLevel.Trace.ToString()).Value.Should().Be("1");
+            SpinWait.SpinUntil(() => this.model.Items.Count == 10, TimeSpan.FromMilliseconds(DelayMilliseconds));
+            this.model.Items.Single(x => x.Key == LogLevel.Trace.ToString()).Value.Should().Be("1");
         }
 
         [Fact]
         public void LoadStatistic_DatesNotDefault_ItemsContainsDates()
         {
             // Arrange
-            var store = new Mock<ILogStore>();
-            StatisticModel model = new StatisticModel(store.Object, "1", "ru");
-
-            store.Setup(
+            this.store.Setup(
                 x => x.CountMessages(It.IsAny<LogLevel>(), It.IsAny<LogLevel>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(0);
 
-            store.Setup(
+            this.store.Setup(
                 x => x.SelectDateUsingFunc("min", LogLevel.Trace, LogLevel.Fatal, null, false)).Returns(DateTime.Now);
 
-            store.Setup(
+            this.store.Setup(
                 x => x.SelectDateUsingFunc("max", LogLevel.Trace, LogLevel.Fatal, null, false)).Returns(DateTime.Now.AddYears(1));
 
-            store.SetupGet(x => x.DatabasePath).Returns(string.Empty);
+            this.store.SetupGet(x => x.DatabasePath).Returns(string.Empty);
 
             // Act
-            model.LoadStatistic();
+            this.model.LoadStatistic();
 
             // Assert
-            SpinWait.SpinUntil(() => model.Items.Count == 12, TimeSpan.FromMilliseconds(DelayMilliseconds));
-            model.Items.Where(x => x.Key == "First").Should().HaveCount(1);
-            model.Items.Where(x => x.Key == "Last").Should().HaveCount(1);
+            SpinWait.SpinUntil(() => this.model.Items.Count == 12, TimeSpan.FromMilliseconds(DelayMilliseconds));
+            this.model.Items.Where(x => x.Key == "First").Should().HaveCount(1);
+            this.model.Items.Where(x => x.Key == "Last").Should().HaveCount(1);
         }
     }
 }
