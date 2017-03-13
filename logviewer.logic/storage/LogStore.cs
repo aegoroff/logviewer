549212e4bee397387c9368fbfdd5353196d8219c
egoroff <egoroff@gmail.com>
2017-03-14 00:24:45 +0300

    begin using c# 7

--- logviewer.logic/storage/LogStore.cs
+++ logviewer.logic/storage/LogStore.cs
@@ -232,8 +232,9 @@ namespace logviewer.logic.storage
             query.Append(" OFFSET ");
             query.Append(offset);
 
-            Action<IDbCommand> beforeRead = command => this.AddParameters(command, min, max, filter, useRegexp, start, finish);
-            Action<IDataReader> onRead = delegate(IDataReader rdr)
+            void BeforeRead(IDbCommand command) => this.AddParameters(command, min, max, filter, useRegexp, start, finish);
+
+            void OnRead(IDataReader rdr)
             {
                 var msg = new LogMessage(rdr[0] as string, rdr[1] as string);
                 // ReSharper disable once ForCanBeConvertedToForeach
@@ -251,8 +252,9 @@ namespace logviewer.logic.storage
                 }
 
                 onReadMessage(msg);
-            };
-            this.connection.ExecuteReader(query.ToString(), onRead, beforeRead, notCancelled);
+            }
+
+            this.connection.ExecuteReader(query.ToString(), OnRead, BeforeRead, notCancelled);
         }
 
         private PropertyType DefinePropertyType(string param)
@@ -288,8 +290,10 @@ namespace logviewer.logic.storage
             }
 
             var query = @"SELECT count(1) FROM Log " + this.Where(min, max, filter, useRegexp, start, finish);
-            Action<IDbCommand> addParameters = cmd => this.AddParameters(cmd, min, max, filter, useRegexp, start, finish);
-            var result = this.connection.ExecuteScalar<long>(query, addParameters);
+
+            void AddParameters(IDbCommand cmd) => this.AddParameters(cmd, min, max, filter, useRegexp, start, finish);
+
+            var result = this.connection.ExecuteScalar<long>(query, AddParameters);
             return result;
         }
 
@@ -307,8 +311,10 @@ namespace logviewer.logic.storage
             var where = this.Where(min, max, filter, useRegexp, DateTime.MinValue, DateTime.MaxValue);
             var query = $@"SELECT {func}({this.dateTimeProperty}) FROM Log {where}";
 
-            Action<IDbCommand> addParameters = cmd => this.AddParameters(cmd, min, max, filter, useRegexp, DateTime.MinValue, DateTime.MaxValue);
-            var result = this.connection.ExecuteScalar<long>(query, addParameters);
+            void AddParameters(IDbCommand cmd) => this.AddParameters(cmd, min, max, filter, useRegexp, DateTime.MinValue, DateTime.MaxValue);
+
+            var result = this.connection.ExecuteScalar<long>(query, AddParameters);
+
             return DateTime.FromFileTime(result);
         }
 
